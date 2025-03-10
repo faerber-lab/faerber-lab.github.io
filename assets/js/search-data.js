6c48d50371e9edd15e99a3a46b1f2fb0bfbaf0b8
michaelfaerber <michael.faerber@tu-dresden.de>
2025-03-10 04:36:44 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@17a443b615d589d00811f156a7708c00dd2ec523 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-member",
+          title: "member",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/member/";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
