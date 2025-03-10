68dfe7259968177a3eeb8117bd5e36f6d2debd05
michaelfaerber <michael.faerber@tu-dresden.de>
2025-03-10 02:57:09 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@5e5c8be2f54a5eed5b5bae08f5d0fe3c63d1e0a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,16 +429,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-starting-as-full-professor-w3-at-scads-ai-dresden-leipzig-tu-dresden",
+          title: 'Starting as full professor (W3) at ScaDS.AI Dresden/Leipzig / TU Dresden.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
