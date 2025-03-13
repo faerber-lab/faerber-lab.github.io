b8aa492a582ad2bf02b47a68ed03ec58ee19c501
michaelfaerber <michael.faerber@tu-dresden.de>
2025-03-13 07:47:30 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@a383c7176652ac02cdb38de56770899afa7c0a19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-vacancies",
           title: "vacancies",
-          description: "Latest openings and available student thesis topics to join our research group.",
+          description: "Latest openings and available student thesis topics.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/vacancies/";
