5a4f96482f3dc117be6b8c4cc6587c6ef8da21b2
michaelfaerber <michael.faerber@tu-dresden.de>
2025-04-16 02:26:36 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@f35cf946bb706ad9ac95804fcc566d3d8e68a087 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-team",
           title: "team",
-          description: "Research group members, including postdocs and PhD students.",
+          description: "Core group members, including postdocs and PhD students.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/team/";
