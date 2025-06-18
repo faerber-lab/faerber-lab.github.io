49e442b7838812037d07d5418dc7dd419e951d68
michaelfaerber <michael.faerber@tu-dresden.de>
2025-06-18 19:53:03 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@d613542c2542f9fd9fbb03101ae830689d47a46e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -553,16 +553,16 @@ ninja.data = [{
           section: "News",},{id: "news-two-papers-have-been-accepted-at-kdd-2025-one-in-the-research-track-and-one-in-the-applied-data-science-track-see-you-in-toronto",
           title: 'Two papers have been accepted at KDD 2025 — one in the Research...',
           description: "",
-          section: "News",},{id: "news-senior-postdoc-position-available-apply-now-to-join-our-team",
-          title: 'Senior PostDoc position available—apply now to join our team!',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/7_postdocE14_2025/";
-            },},{id: "news-junior-postdoc-position-available-apply-now-to-join-our-team",
+          section: "News",},{id: "news-junior-postdoc-position-available-apply-now-to-join-our-team",
           title: 'Junior PostDoc position available—apply now to join our team!',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/8_postdocE13_2025/";
+            },},{id: "news-senior-postdoc-position-available-apply-now-to-join-our-team",
+          title: 'Senior PostDoc position available—apply now to join our team!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/7_postdocE14_2025/";
             },},{id: "projects-klartext",
           title: 'Klartext',
           description: "AI-based Translation of Websites Into Plain Language",
