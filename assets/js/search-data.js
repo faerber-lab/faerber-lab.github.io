d571f670b1de695a4605de82536c101f47485b73
michaelfaerber <michael.faerber@tu-dresden.de>
2025-06-18 17:37:48 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@b4a1c41c1dd5757dfcc82f3eb574a0a10f4c63d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -557,7 +557,12 @@ ninja.data = [{
           title: 'Senior PostDoc position available—apply now to join our team!',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/7_postdoc_2025/";
+              window.location.href = "/news/7_postdocE14_2025/";
+            },},{id: "news-junior-postdoc-position-available-apply-now-to-join-our-team",
+          title: 'Junior PostDoc position available—apply now to join our team!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/8_postdocE13_2025/";
             },},{id: "projects-klartext",
           title: 'Klartext',
           description: "AI-based Translation of Websites Into Plain Language",
