0ce149d0958fd0066db389a536148f3ec87dcb26
michaelfaerber <michael.faerber@tu-dresden.de>
2025-07-26 07:32:17 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@b6a624b2a26669e1334baffa668340abd42d1c0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -289,17 +289,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/echarts/";
         
       },
-    },{id: "post-a-post-with-tikzjax",
-      
-        title: "a post with TikZJax",
-      
-      description: "this is what included TikZ code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/tikzjax/";
-        
-      },
     },{id: "post-a-post-with-bibliography",
       
         title: "a post with bibliography",
