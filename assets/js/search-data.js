924bbb49498fcd8b1fb8505f4856913be37e049f
michaelfaerber <michael.faerber@tu-dresden.de>
2025-09-19 14:00:16 +0000

    Deploying to gh-pages from @ faerber-lab/faerber-lab.github.io@6b58ff28c371c93238a99f37f43f89e1796e0245 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -226,7 +226,10 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/8_postdocE13_2025/";
-            },},{id: "news-three-papers-have-been-accepted-at-emnlp-2025-see-you-in-suzhou-china",
+            },},{id: "news-i-gave-a-keynote-on-llms-and-knowledge-graphs-for-science-from-papers-to-insights-at-semantics-2025-see-you-in-vienna",
+          title: 'I gave a keynote on “LLMs and Knowledge Graphs for Science: From Papers...',
+          description: "",
+          section: "News",},{id: "news-three-papers-have-been-accepted-at-emnlp-2025-see-you-in-suzhou-china",
           title: 'Three papers have been accepted at EMNLP 2025 ! See you in Suzhou,...',
           description: "",
           section: "News",},{id: "projects-klartext",
